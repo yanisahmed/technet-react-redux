89e62831ef4a92111ac00480d2f2b810a6f8ea83
Yanis Ahmed <yanisahmed12@gmail.com>
2024-01-22 08:58:46 +0600

    redux store added

---
+++ src/redux/features/cart/cartSlice.tsx
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const cardSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {},
+});
+
+export default cardSlice.reducer;
