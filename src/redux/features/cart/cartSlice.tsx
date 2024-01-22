44f98f6a49842b8462e7f748fed1e3bf69466897
Yanis Ahmed <yanisahmed12@gmail.com>
2024-01-22 09:18:13 +0600

    addToCard reducer added

--- src/redux/features/cart/cartSlice.tsx
+++ src/redux/features/cart/cartSlice.tsx
@@ -1,9 +1,21 @@
+import { IProduct } from '@/types/globalTypes';
 import { createSlice } from '@reduxjs/toolkit';
+
+interface ICart {
+  products: IProduct[];
+}
+const initialState: ICart = {
+  products: [],
+};
 
 export const cardSlice = createSlice({
   name: 'cart',
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action: payloadAction<IProduct>) => {
+      state.products.push(action.payload);
+    },
+  },
 });
-
+export const { addToCart } = cardSlice.actions;
 export default cardSlice.reducer;
